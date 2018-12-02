28b5d9a5eb2deb478f9b4a19b3a2cd0eec52f3d9
danny crasto <danwald79@gmail.com>
2018-12-02 15:39:59 +0400

    adds express with static

---
+++ nodejs/xpress/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('/', function(req, res, next){
+    console.log("Request: " + req.url);
+    next();
+});
+
+app.get('/', function(req, res) {
+res.send('<html>' +
+         '<head><link href=assets/style.css type=text/css rel=stylesheet /></head>' + 
+         '<body><h1>hello world!!</h1></body></html>')
+});
+
+app.get('/person/:id', function(req, res) {
+res.send('<html><body><h1>Person: ' + 
+req.params.id + '</h1></body></html>');
+});
+
+app.get('/api', function(req, res) {
+    res.json({firstname: 'dan', lastname: 'wald'});
+});
+
+
+app.listen(port);
