dc98c2c07da2176bff1a6a50d4e442d6bf8c3e9c
danny crasto <danwald79@gmail.com>
2018-12-10 06:26:37 +0400

    fixes css

--- nodejs/angular/app.js
+++ nodejs/angular/app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use('/assets', express.static(__dirname + '/public'));
+app.use('/asset', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.render('index');
