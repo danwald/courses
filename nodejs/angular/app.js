3af4b6198cb711ef4c949ee138f3f96ce12a383b
danny crasto <danwald79@gmail.com>
2018-12-10 06:58:47 +0400

    Updates angular app with server side data

--- nodejs/angular/app.js
+++ nodejs/angular/app.js
@@ -1,11 +1,21 @@
 var express = require('express')
 var app = express();
 var port = process.env.PORT || 3000;
+var people = [{
+	name: 'dan'
+	},
+	{
+	name: 'wald'
+	},
+	{
+        name: 'foo'
+    }
+];
 
 app.set('view engine', 'ejs');
 app.use('/asset', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', { serverPeople: people });
 });
 app.listen(port);
--- nodejs/angular/public/js/app.js
+++ nodejs/angular/public/js/app.js
@@ -3,15 +3,5 @@ angular.module('TestApp')
     .controller('MainController', ctrlFunc);
 
 function ctrlFunc(){
-    this.message = 'Hello';
-    this.people = [
-    {
-        name: 'dan'
-    },
-    {
-        name: 'wald'
-    },
-    {
-        name: 'foo'
-    }];
+    this.people = clientPeople;
 }
