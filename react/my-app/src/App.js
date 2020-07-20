bcc2d77dabdf38bc6550db159b8ae129fe45fefa
danny crasto <danwald79@gmail.com>
2020-07-20 07:25:21 +0400

    Using jsx over react.createElement

---
+++ react/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hi, I'm a React App</h1>
+        </div>
+    );
+    //return React.createElement(
+    //    'div', {className: 'App'}, 
+    //    React.createElement('h1', null, 'Hello react world')
+    //);
+}
+
+export default App;
