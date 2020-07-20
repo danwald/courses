ee0b534ea52076269bc9a14e071e2e604dbd7f92
danny crasto <danwald79@gmail.com>
2020-07-20 07:48:57 +0400

    add state and converted to react component

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+class App extends Component {
+    state = {
+        persons: [
+            { name: 'Danny', age: '42' },
+            { name: 'foo', age: '100' }
+        ]
+    }
+
+    render() {
     return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
-            <Person name='Danny' age='42'>like chocolate</Person>
-            <Person name='foo' age='100'/>
+            <button>Switch Name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>like chocolate</Person>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         </div>
 
     );
@@ -16,6 +24,7 @@ function App() {
     //    'div', {className: 'App'}, 
     //    React.createElement('h1', null, 'Hello react world')
     //);
+    }
 }
 
 export default App;
