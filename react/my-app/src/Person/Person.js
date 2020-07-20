086ff9aca68f0cbefd95eb46dc663a7ad59c0193
danny crasto <danwald79@gmail.com>
2020-07-20 07:41:32 +0400

    passing in content to function

--- react/my-app/src/Person/Person.js
+++ react/my-app/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p>Hello I am danny. Iam {Math.floor(Math.random()*100)} years old</p>
+const person = (props) => {
+    return(
+        <div>
+        <p>Hello I am {props.name} I am {props.age} years old</p>
+        <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
