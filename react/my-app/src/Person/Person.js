0ed402ebef5147b0859f2c82a38e8ef161811bac
danny crasto <danwald79@gmail.com>
2020-07-20 07:35:34 +0400

    dynamic component with js Math.random

--- react/my-app/src/Person/Person.js
+++ react/my-app/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p>Hello I am danny</p>
+    return <p>Hello I am danny. Iam {Math.floor(Math.random()*100)} years old</p>
 }
 
 export default person;
