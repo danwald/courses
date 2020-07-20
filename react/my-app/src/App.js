6d50279400f4ed4d8f46e1a530f84aa0916ae516
danny crasto <danwald79@gmail.com>
2020-07-20 07:31:50 +0400

    added a person component

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
     return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
+            <Person />
         </div>
+
     );
     //return React.createElement(
     //    'div', {className: 'App'}, 
