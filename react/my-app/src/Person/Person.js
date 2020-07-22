243a846c58ced824ed09652ae95f4f9e09fb0cd2
danny crasto <danwald79@gmail.com>
2020-07-22 07:56:19 +0400

    added css and paramterized handlers

--- react/my-app/src/Person/Person.js
+++ react/my-app/src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) => {
     return(
-        <div>
-        <p>Hello I am {props.name} I am {props.age} years old</p>
+        <div className="Person">
+        <p onClick={props.click}>
+            Hello I am {props.name} I am {props.age} years old</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 }
