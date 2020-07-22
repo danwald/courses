8f95313f8753c1dc06cb9dbc6699e466fa6a33ba
danny crasto <danwald79@gmail.com>
2020-07-22 07:56:19 +0400

    added css and paramterized handlers

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -10,23 +10,48 @@ class App extends Component {
         ]
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         //console.log('Was Clicked!');
         this.setState({
             persons: [
-                { name: 'Daniel', age: '23' },
+                { name: newName, age: '23' },
                 { name: 'foobar', age: '10' }
             ]
         })
     }
 
+    nameChangeHander = (event) => {
+        this.setState({
+            persons: [
+                { name: 'Danny', age: '23' },
+                { name: event.target.value, age: '10' }
+            ]
+        })
+    }
+
     render() {
+        const style= {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1x solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
     return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
-            <button onClick={this.switchNameHandler}>Switch Name</button>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>like chocolate</Person>
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+            <button 
+                onClick={this.switchNameHandler.bind(this, 'Foo')}
+                style={style}>
+            Switch Name</button>
+            <Person 
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age}>like chocolate</Person>
+            <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Bar!')}
+                changed={this.nameChangeHander}/>
         </div>
 
     );
