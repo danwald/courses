a9d766f227006953afb6f7a8edc53c69c6c28109
danny crasto <danwald79@gmail.com>
2020-07-25 15:46:40 +0400

    adding assignement-001 for basic syntax

---
+++ react/base-syntax-assignment-001/src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './UserInput.css';
+
+const userinput = (props) => {
+			return(
+								<div className="UserInput">
+								<input type="text">Inputtext</input>
+								</div>
+						)
+}
+
+export default userinput;
---
+++ react/base-syntax-assignment-001/src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './UserOutput.css';
+
+const useroutput = (props) => {
+			return(
+								<div className="UserOutput">
+								<p>
+								</p>
+								</div>
+						)
+}
+
+export default useroutput;
