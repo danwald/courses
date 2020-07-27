4571afb5bf21276bc6a287b18a2948f76575e5c4
danny crasto <danwald79@gmail.com>
2020-07-27 07:21:53 +0400

    finished assignement 1

--- react/base-syntax-assignment-001/src/UserInput/UserInput.js
+++ react/base-syntax-assignment-001/src/UserInput/UserInput.js
@@ -3,11 +3,12 @@ import React from 'react';
 import './UserInput.css';
 
 const userinput = (props) => {
-			return(
-								<div className="UserInput">
-								<input type="text">Inputtext</input>
-								</div>
-						)
+	return(
+		<div className="UserInput">
+			<label htmlFor="in">Input:</label>
+			<input id="in" type="text" onChange={props.changed} value={props.username}/>
+		</div>
+	)
 }
 
 export default userinput;
