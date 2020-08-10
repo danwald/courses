3e47e80f998ab455edd666da9943a7ba3439e422
danny crasto <danwald79@gmail.com>
2020-08-10 21:08:09 +0400

    adds multiple eventable buttons

--- js/js-master-class/html5JS/index.js
+++ js/js-master-class/html5JS/index.js
@@ -1,4 +1,11 @@
 window.onload = function()  {
 	const elmain = document.getElementById("main");
 	elmain.style.color = "red";
+
+	const buttons = document.querySelectorAll("button");
+	buttons.forEach( button => {
+		button.addEventListener("click", e => {
+			console.log(e.target.innerHTML);
+		});
+	});
 };
