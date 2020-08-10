dee7914565171ebcfc8ab1fef6a5142d6f91747b
danny crasto <danwald79@gmail.com>
2020-08-10 21:15:05 +0400

    adds settimeout to remove click handler

--- js/js-master-class/html5JS/index.js
+++ js/js-master-class/html5JS/index.js
@@ -2,10 +2,17 @@ window.onload = function()  {
 	const elmain = document.getElementById("main");
 	elmain.style.color = "red";
 
+	const clickHandler = e => {
+		console.log(e.target.innerHTML);
+	}
 	const buttons = document.querySelectorAll("button");
 	buttons.forEach( button => {
-		button.addEventListener("click", e => {
-			console.log(e.target.innerHTML);
-		});
+		button.addEventListener("click", clickHandler);
 	});
+
+	setTimeout(() => {
+		buttons.forEach( button => {
+			button.removeEventListener("click", clickHandler);
+		});
+	},3000);
 };
