4560b81c6294dbaa75e7cd031309bd7342df8db9
danny crasto <danwald79@gmail.com>
2021-01-18 20:45:25 +0400

    added menu component 1

--- react/coursera-confusion/src/components/MenuComponent.js
+++ react/coursera-confusion/src/components/MenuComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Media } from reactstrap;
+import { Media } from 'reactstrap';
 
 class Menu extends Component {
 
-	constructor(props}{
+	constructor(props) {
 		super(props);
 		this.state = {
 			dishes: [
@@ -45,19 +45,34 @@ class Menu extends Component {
 			}
     ]
 	};
-
-	render() {
-		const menu;
-		return (
-			<div className="container">
-				<div className="row">
-					<Media>
-						{menu}
-					</Media>
-				</div>
-			</div>
-		);
 	}
+    render() {
+        const menu = this.state.dishes.map((dish) => {
+            return (
+              <div key={dish.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={dish.image} alt={dish.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{dish.name}</Media>
+                    <p>{dish.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
+        });
+
+        return (
+          <div className="container">
+            <div className="row">
+              <Media list>
+                  {menu}
+              </Media>
+            </div>
+          </div>
+        );
+    }
 }
 
 export default Menu;
