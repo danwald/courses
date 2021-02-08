aeb59a0c2abcee64113deaeb0435812861cd2aa5
danny crasto <danwald79@gmail.com>
2021-02-08 20:40:35 +0400

    go onclick to work

--- react/coursera-confusion/src/components/MenuComponent.js
+++ react/coursera-confusion/src/components/MenuComponent.js
@@ -11,7 +11,7 @@ class Menu extends Component {
       const menu = this.props.dishes.map((dish) => {
           return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-              <Card>
+              <Card onClick={() => this.props.onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay body className="ml-5">
                   <CardTitle heading>{dish.name}</CardTitle>
