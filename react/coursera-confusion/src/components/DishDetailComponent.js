4bafbba17218cb67b4953f58065c6aa465b8abee
danny crasto <danwald79@gmail.com>
2021-02-08 21:22:42 +0400

    converted Menu|Dishdetail component to Func from Class based components

--- react/coursera-confusion/src/components/DishDetailComponent.js
+++ react/coursera-confusion/src/components/DishDetailComponent.js
@@ -1,49 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
-class DishDetail extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			dish: null,
-		}
-		console.log('DishDetail Component constructor');
-	}
-
-	componentDidMount() {
-		console.log('DishDetail Component componentDidMount');
-	}
-
-	componentDidUpdate() {
-		console.log('DishDetail Component componentDidUpdate');
-	}
-
-	render() {
-		console.log('DishDetail Component render');
-		if(this.props.dish != null){
-			return(
-		  	<div className="row">
-					<Card>
-						<CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-						<CardBody>
-							<CardTitle>{this.props.dish.name}</CardTitle>
-							<CardText>{this.props.dish.description}</CardText>
-													</CardBody>
-					</Card>
-					<div className="col-12 col-md-5 m-1">
-								<ul class="list-inline">
-									<li class="list-inline-item">Lorem ipsum</li>
-									<li class="list-inline-item">Phasellus iaculis</li>
-									<li class="list-inline-item">Nulla volutpat</li>
-								</ul>
-					</div>
+const DishDetail = ({dish}) => {
+	if(dish != null){
+		return(
+		  <div className="row">
+				<Card>
+					<CardImg width="100%" src={dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+												</CardBody>
+				</Card>
+				<div className="col-12 col-md-5 m-1">
+							<ul class="list-inline">
+								<li class="list-inline-item">Lorem ipsum</li>
+								<li class="list-inline-item">Phasellus iaculis</li>
+								<li class="list-inline-item">Nulla volutpat</li>
+							</ul>
 				</div>
-			);
-		}
-		else {
-			return(<div></div>);
-		}
+			</div>
+		);
+	}
+	else {
+		return(<div></div>);
 	}
 }
 
