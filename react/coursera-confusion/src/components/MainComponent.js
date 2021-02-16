f04076165d306d09946dd91f9ce72b8dfebc81fe
danny crasto <danwald79@gmail.com>
2021-02-16 23:28:35 +0400

    adding props to home

--- react/coursera-confusion/src/components/MainComponent.js
+++ react/coursera-confusion/src/components/MainComponent.js
@@ -4,7 +4,11 @@ import DishDetail from './DishDetailComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import {Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -14,6 +18,9 @@ class Main extends Component {
 		super(props);
 		this.state ={
 			dishes: DISHES,
+			comments: COMMENTS,
+			leaders: LEADERS,
+			promotions: PROMOTIONS,
 		};
 	}
 
@@ -21,7 +28,11 @@ class Main extends Component {
 		const HomePage = () => {
 			return(
 				<div>
-					<Home />
+					<Home
+						dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+						leader={this.state.leaders.filter((dish) => dish.featured)[0]}
+					 	promotions={this.state.dishes.filter((dish) => dish.featured)[0]}
+					 />
 				</div>
 			);
 		}
@@ -31,6 +42,7 @@ class Main extends Component {
 				<Switch>
 					<Route path="/home" component={HomePage} />
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+					<Route exact path="/contactus" component={Contact} />
 					<Redirect to="/home" />
 				</Switch>
 			  <DishDetail
