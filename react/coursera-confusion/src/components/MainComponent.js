8f919ca18b0511577cb5b1c4885048cbd0d51d23
danny crasto <danwald79@gmail.com>
2021-02-22 21:17:42 +0400

    downloaded/integrated AboutUs comp

--- react/coursera-confusion/src/components/MainComponent.js
+++ react/coursera-confusion/src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutUsComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -51,6 +52,7 @@ class Main extends Component {
 				<Header />
 				<Switch>
 					<Route path="/home" component={HomePage} />
+					<Route exact path="/about" component={() => <About leaders={this.state.leaders} />} />
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
 					<Route path="/menu/:dishId" component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
