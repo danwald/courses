a6898661f81043d32b85ec3d24a88c05e23e7be2
danny crasto <danwald79@gmail.com>
2021-04-05 20:57:10 +0400

    fix comment

--- react/coursera-confusion/src/redux/comments.js
+++ react/coursera-confusion/src/redux/comments.js
@@ -9,8 +9,6 @@ export const Comments = (state = {
 	    switch (action.type) {
 				case ActionTypes.ADD_COMMENT:
 					var comment = action.payload;
-					comment.id = state.commnets.length;
-					comment.date = new Date().toISOString();
 					return {...state, comments: state.comments.concat(comment)};
 
 				case ActionTypes.ADD_COMMENTS:
