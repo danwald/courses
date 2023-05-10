fe4ff5929a9d3fb43c0379680bc735b6032f35f2
danny crasto <danwald79@gmail.com>
2023-05-10 22:30:35 +0400

    adds example of attrib lookup

---
+++ python/attrib.py
@@ -0,0 +1,41 @@
+import dataclasses
+import logging
+
+logging.basicConfig(level='INFO')
+logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class DictAttrib:
+    name:  str  # attribute
+    data:  dict = dataclasses.field(default_factory=dict)  # user defined attributes
+
+    def __str__(self):
+        return f'{self.name}| {self.data}'
+
+    def __getattr__(self, name):
+        """called when attribute not found see __getattribute__"""
+        try:
+            # check if in dict
+            return self.data[name]
+        except KeyError:
+            # to avoid nested exception
+            pass
+        raise AttributeError(name)
+
+
+if __name__ == '__main__':
+    foo = DictAttrib('foo')
+    bar = DictAttrib('bar', data={'bar': 'hello'})
+    try:
+        logger.info(f'{foo}')
+        logger.info(f'{foo.bar}')
+    except AttributeError:
+        logger.exception('failure')
+
+    try:
+        logger.info(f'{bar}')
+        logger.info(f'{bar.bar}')
+        logger.info(f'{bar.a}')
+    except AttributeError:
+        logger.exception('failed')
