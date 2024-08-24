a5e7c106249c6ea04f68c7997b8f5a755974aaf8
dcrasto <danny@dubizzle.com>
2024-08-24 18:00:05 +0400

    adds orderbook

--- ints/cnrts/bookie/bookie/data.py
+++ ints/cnrts/bookie/bookie/data.py
@@ -11,3 +11,36 @@ class Bid:
 @dataclass
 class Ask(Bid):
     pass
+
+class OrderBook:
+    def __init__(self, asks=None, bids=None):
+        self.asks = asks or []
+        self.bids = bids or []
+        self.index = 0
+
+    def __iadd__(self, other):
+        self.asks.extend(other.asks)
+        self.bids.extend(other.bids)
+        self.asks = sorted(self.asks, key=lambda x: x.price)
+        self.bids = sorted(self.bids, key=lambda x: x.price, reverse=True)
+        self.index = 0
+        return self
+
+    def __add__(self, other):
+        self.asks = sorted(self.asks.extend(other.asks), key=lambda x: x.price)
+        self.bids = sorted(self.bids.extend(other.bids), key=lambda x: x.price, reverse=True)
+        self.index = 0
+        return self
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            ask, bid = self.asks[self.index], self.bids[self.index]
+        except IndexError:
+            self.index = 0
+            raise StopIteration
+        else:
+            self.index += 1
+            return ask, bid
