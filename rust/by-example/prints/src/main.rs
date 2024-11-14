9f4b76f31638b1ac039a42688e54c1c947701691
danwald <danwald79@gmail.com>
2024-11-14 21:59:52 +0400

    does prints

---
+++ rust/by-example/prints/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    eprintln!("Hello, world!");
+    let dan = "dan";
+    println!("Hello {dan}, hello world!");
+    println!("Hello {}, hello world!", "peeps");
+    println!("Hello {1} {}, hello world!", "peeps", 2);
+}
