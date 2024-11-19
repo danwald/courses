bb8e97f476f882c0c032118b240dab0f838ea36a
danwald <danwald79@gmail.com>
2024-11-19 21:35:00 +0400

    Adds fmt iml for Point2D

--- rust/by-example/prints/src/main.rs
+++ rust/by-example/prints/src/main.rs
@@ -1,8 +1,24 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     eprintln!("Hello, world!");
     let dan = "dan";
     println!("Hello {dan}, hello world!");
     println!("Hello {}, hello world!", "peeps");
-    println!("Hello {1} {}, hello world!", "peeps", 2);
+    println!("Hello {1} {:?}, hello world!", "peeps", 2);
+    let p = Point2D {y:1.4, x:2.4};
+    println!("Debug Point: {:?}", p);
+    println!("Point: {}", p);
 }
