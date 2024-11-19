af2c3927826ce39353a6d304a4c7163317ac4b4f
danwald <danwald79@gmail.com>
2024-11-19 21:51:00 +0400

    Adds city format

--- rust/by-example/prints/src/main.rs
+++ rust/by-example/prints/src/main.rs
@@ -1,16 +1,28 @@
-use std::fmt;
+use std::fmt::{Display, Formatter, Result};
 
 #[derive(Debug)]
 struct Point2D {
     x: f64,
     y: f64,
 }
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl Display for Point2D {
+    fn fmt(&self, f: &mut Formatter) -> Result {
         write!(f, "({},{})", self.x, self.y)
     }
 }
 
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let lac = if self.lat < 0.0 {'S'} else {'N'};
+        let loc = if self.lon < 0.0 {'W'} else {'E'};
+        write!(f, "{} {:.3}{} {:.3}{}", self.name, self.lat.abs(), lac, self.lon.abs(), loc)
+    }
+}
 fn main() {
     println!("Hello, world!");
     eprintln!("Hello, world!");
@@ -21,4 +33,8 @@ fn main() {
     let p = Point2D {y:1.4, x:2.4};
     println!("Debug Point: {:?}", p);
     println!("Point: {}", p);
+    let c = City {name: "Dubai", lat: 25.276987, lon: 55.296249};
+    let d = City {name: "Dublin", lat: 53.349805, lon: -6.260310};
+    println!("{}", c);
+    println!("{}", d);
 }
