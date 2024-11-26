c0bfba37750ad64910279e0a4b9ec23156996702
danwald <danwald79@gmail.com>
2024-11-26 22:04:48 +0400

    Adds matrix transpose

--- rust/by-example/prims/src/main.rs
+++ rust/by-example/prims/src/main.rs
@@ -1,7 +1,30 @@
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32,f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "({},{})\n({},{})", self.0, self.1, self.2, self.3)
+    }
+
+}
+fn reverse(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
+
+fn transpose(matrix: Matrix) -> Matrix {
+   reverse(matrix)
+}
+
 fn main() {
     let _int: i64 = 10000;
     let _float = 3.14f64;
     let _bool = false;
     let _tup = (1, 2, 4);
     let mut _ar: [i32; 5] = [1, 2, 3, 4, 5];
+    let m = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", m);
+    println!("Matrix:\n{}", m);
+    println!("Transpose Matrix:\n{}", transpose(m));
 }
