072aae203b7d09a55593e81728b1ea6ceb30307d
danwald <danwald79@gmail.com>
2024-12-19 21:55:25 +0400

    Added print and area for rectangle

--- rust/by-example/structs/src/main.rs
+++ rust/by-example/structs/src/main.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+
+use::std::fmt::{Display, Formatter, Result};
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -21,6 +24,23 @@ struct Rectangle {
     bottom_right: Point,
 }
 
+impl Rectangle {
+
+    fn area(&self) -> f32 {
+        let Point{x: x1, y: y1} = self.top_left;
+        let Point{x: x2, y: y2} = self.bottom_right;
+        ((x1 - x2) * (y1 - y2)).abs()
+    }
+}
+
+impl Display for Rectangle {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "top_left: ({}, {}), bottom_right: ({}, {})", self.top_left.x, self.top_left.y, self.bottom_right.x, self.bottom_right.y)
+    }
+}
+
+
+
 fn main() {
     let name = String::from("danny");
     let age = 45;
@@ -31,10 +51,12 @@ fn main() {
     println!("Pair:{:?},{:?}", pair.0, pair.1);
 
     let tl = Point{x: 0.0, y: 5.0};
-    let br = Point{x: -5.0, y: 5.0};
+    let br = Point{x: -5.0, y: -5.0};
     println!("tl {:?}, br {:?}", tl, br);
 
-    let rect = Rectangle{top_left: tl, bottom_right: br};
     let Pair(p1, p2) = pair;
     println!("p1: {}, p2: {}", p1, p2);
+
+    let rect = Rectangle{top_left: tl, bottom_right: br};
+    println!("{} {}^2", rect, rect.area());
 }
