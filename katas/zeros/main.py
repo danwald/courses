41d86343fa89336c35f78314c5d1d3cc97fafa67
danwald <danwald79@gmail.com>
2025-02-19 06:31:05 +0400

    move zeros in place

--- katas/zeros/main.py
+++ katas/zeros/main.py
@@ -2,7 +2,44 @@
 
 
 def move_inplace(array):
-    return []
+    def next_val(array, idx, val, neg=False):
+        while idx < len(array):
+            ev = array[idx] == val
+            if neg:
+                ev = not ev
+            if ev:
+                break
+            else:
+                idx += 1
+        return idx if idx < len(array) else idx - 1
+
+    if len(array) < 2:
+        return array
+
+    def swap(array, i, j, swap=True):
+        if swap:
+            array[i], array[j] = array[j], array[i]
+        else: # head insert
+            assert i < j
+            iv = array[j]
+            array[i+1:j+1] = array[i:j]
+            array[i] = iv
+
+    i = 1
+    while i < len(array):
+        pi = i-1
+        #print(array, pi, i)
+        pv, nv = array[pi], array[i]
+        if pv:
+            if not nv:
+                swap(array, pi, i, False)
+            else:
+                j = next_val(array, i, 0)
+                #print(j)
+                if not array[j]:
+                    swap(array, pi, j, False)
+        i += 1
+    return array
 
 
 def move_zeros(array):
@@ -17,9 +54,9 @@ def move_zeros(array):
             else:
                 return array
 
-    z, i = [],[]
+    z, i = [], []
     for x in array:
-        if x == 0:
+        if not x:
             z.append(x)
         else:
             i.append(x)
@@ -27,17 +64,19 @@ def move_zeros(array):
     return z
 
 
-
 if __name__ == '__main__':
     for mt in (
         move_zeros,
         move_inplace,
     ):
-        assert [] ==  mt([])
-        assert [0] ==  mt([0])
-        assert [0,1] ==  mt([1,0])
-        assert [0,1] ==  mt([0,1])
-        assert [0,0,1,2] ==  mt([1,2,0,0])
-        assert [0,0,0,0] ==  mt([0,0,0,0])
-        assert [0,0,1,2] ==  mt([0,0,1,2])
-        assert [3,4,1,2] ==  mt([3,4,1,2])
+        print(f'testing: {mt.__name__}')
+        #assert [] ==  mt([])
+        #assert [0] ==  mt([0])
+        #assert [0,1] ==  mt([1,0])
+        #assert [0,1] ==  mt([0,1])
+        #assert [0,0,1,2] ==  mt([1,2,0,0])
+        #assert [0,0,0,0] ==  mt([0,0,0,0])
+        #assert [0,0,1,2] ==  mt([0,0,1,2])
+        #assert [3,4,1,2] ==  mt([3,4,1,2])
+        #assert [0,0,1,2] ==  mt([0,1,0,2])
+        assert [0,0,0,0,1,2,3,5] ==  mt([0,1,2,0,0,3,0,5])
