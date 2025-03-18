84c6e7e7d2177d5b6f2abdb707b96c313af32455
danny crasto <danwald79@gmail.com>
2025-03-18 14:41:33 +0400

    04 weather
    Initial copy pasta w/o refactor

---
+++ katas/04_datam/main.py
@@ -0,0 +1,50 @@
+#http://codekata.com/kata/kata04-data-munging/
+'''
+Part One: Weather Data
+In weather.dat you’ll find daily weather data for Morristown, NJ for June 2002. Download this text file, then write a program to output the day number (column one) with the smallest temperature spread (the maximum temperature is the second column, the minimum the third column).
+
+Part Two: Soccer League Table
+The file football.dat contains the results from the English Premier League for 2001/2. The columns labeled ‘F’ and ‘A’ contain the total number of goals scored for and against each team in that season (so Arsenal scored 79 goals against opponents, and had 36 goals scored against them). Write a program to print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+'''
+
+
+def weather(infile='weather.dat'):
+    min_spread, mday = 1 << 10, -1
+    with open(infile) as fp:
+        _ = fp.readline()
+        for ln in fp:
+            try:
+                day, mx, mi = map(lambda x: int(x),  ln.split()[:3])
+            except:
+                pass
+            else:
+                sp = mx - mi
+                if sp < min_spread:
+                    min_spread, mday = sp, day
+        return mday
+
+
+def soccer(infile='football.dat'):
+    min_spread, wteam = 1 << 10, None
+    with open(infile) as fp:
+        _ = fp.readline()
+        for ln in fp:
+            try:
+                data = ln.split()
+                team,gf,ga = data[1], int(data[-4]), int(data[-2])
+            except:
+                pass
+            else:
+                sp = abs(gf - ga)
+                if sp < min_spread:
+                    print(team, sp, gf, ga)
+                    min_spread, wteam = sp, team
+        return wteam
+
+
+
+if __name__ == '__main__':
+    for method in (weather, soccer):
+        print(
+            'Running: ', method.__name__, 'result: ', method()
+        )
