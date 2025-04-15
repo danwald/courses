7aaa43a605a0ceee3a0043d8977327d66ccaf0b5
danny crasto <danwald79@gmail.com>
2025-04-15 15:28:05 +0400

    query params

--- fastapi/tuit/main.py
+++ fastapi/tuit/main.py
@@ -1,12 +1,16 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 
 class ModelClass(str, Enum):
     foo = "foo"
     bar = "bar"
 
+fake_items_db = {
+        "items":["foo", "bar", "baz",],
+        "db": dict(one=1, two=2, three=3),
+}
 
 app = FastAPI()
 
@@ -30,3 +34,14 @@ async def models(model_name: ModelClass):
             return {"message": f"Fooie {model_name.value}"}
         case ModelClass.bar:
             return {"message": f"barie {model_name.value}"}
+
+@app.get("/items/{item_id}")
+async def items(item_id: str):
+    try:
+        return {"message": fake_items_db['db'][item_id]}
+    except KeyError:
+        raise HTTPException(status_code=404, detail=f"Item not found for key {item_id}")
+
+@app.get("/items/")
+async def get_item(skip: int=0, limit: int=10):
+    return fake_items_db['items'][skip:skip+limit]
