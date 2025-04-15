e539aa405a4af7a23cf8b8c120ba16e53ce19300
danny crasto <danwald79@gmail.com>
2025-04-15 14:50:17 +0400

    params

--- fastapi/tuit/main.py
+++ fastapi/tuit/main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,8 +6,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": f"Hello world"}
 
-@app.get("/{name}")
-async def root2(name):
-    return {"message": f"Hello {name}"}
+@app.get("/hello/{param}")
+async def hello_str(param: str):
+    return {"message": f"Hello {param}"}
+
+@app.get("/age/{age}")
+async def hello_arg(age: int):
+    return {"message": f"You're {age} years old"}
