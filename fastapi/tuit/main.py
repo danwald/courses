68a17c1090e1d206aa053ed6fb46c3f189b7cb2b
danny crasto <danwald79@gmail.com>
2025-04-29 21:02:24 +0400

    SQLModel heros

--- fastapi/tuit/main.py
+++ fastapi/tuit/main.py
@@ -9,7 +9,8 @@ from fastapi import FastAPI, HTTPException, Query, Path, Body, Cookie, Header, F
 from pydantic import BaseModel, AfterValidator, Field, HttpUrl
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
-
+from sqlmodel import Field as SQLField
+from sqlmodel import Session, SQLModel, create_engine, select
 
 from passlib.context import CryptContext
 import jwt
@@ -123,7 +124,45 @@ class FilterParams(BaseModel):
     tags:  set[str] = set()
 
 
+class HeroBase(SQLModel):
+    name: str = SQLField(index=True)
+    age: int | None = SQLField(default=None, index=True)
+
+class Hero(HeroBase, table=True):
+    id: int | None = SQLField(default=None, primary_key=True)
+    secret_name: str
+
+class HeroPublic(HeroBase):
+    id: int
+
+class HeroCreate(HeroBase):
+    secret_name: str
+
+class HeroUpdate(HeroBase):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+SessionDep = Annotated[Session, Depends(get_session)]
+
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 app.add_middleware(
     CORSMiddleware,
@@ -333,3 +372,51 @@ async def upload_file(files: list[UploadFile]) -> dict[str, list[str]]:
 @app.get("/auth/items", tags=["Auth"])
 async def get_auth_items(token: Annotated[str, Depends(o2_scheme_pass)]) -> dict[str, str]:
     return {"token": token}
+
+@app.post("/heroes/", tags=['Heros'], response_model=HeroPublic)
+def create_hero(hero: HeroCreate, session: SessionDep):
+    db_hero = Hero.model_validate(hero)
+    session.add(db_hero)
+    session.commit()
+    session.refresh(db_hero)
+    return db_hero
+
+
+@app.get("/heroes/", tags=["Heros"], response_model=list[HeroPublic])
+def read_heroes(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get("/heroes/{hero_id}", tags=["Heros"], response_model=HeroPublic)
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
+@app.delete("/heroes/{hero_id}", tags=["Heros"])
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
+
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic, tags=["Heros"])
+def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+    return hero_db
