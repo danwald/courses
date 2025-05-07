d7976a59a85127f4b2ff5c964f9454b8e9bee687
danny crasto <danwald79@gmail.com>
2025-05-07 20:02:30 +0400

    WS with handles disconnect

--- fastapi/tuit/test_main.py
+++ fastapi/tuit/test_main.py
@@ -1,10 +1,25 @@
+import pytest
 from fastapi.testclient import TestClient
+from fastapi.websockets import WebSocket
 
 from main import app
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
-def test_read_main():
+def test_read_main(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world"}
+
+@app.websocket("/ws")
+async def websocket(websocket: WebSocket):
+    await websocket.accept()
+    await websocket.send_json({"msg": "Hello WebSocket"})
+    await websocket.close()
+
+def test_websocket(client):
+    with client.websocket_connect("/ws") as websocket:
+        data = websocket.receive_json()
+        assert data == {"msg": "Hello WebSocket"}
