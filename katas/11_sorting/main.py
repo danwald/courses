293dd140f9e2b9172cc7f538ac251520b9b2dddd
danny crasto <danwald79@gmail.com>
2025-05-14 15:46:13 +0400

    initial implementation of insertion sort

--- katas/11_sorting/main.py
+++ katas/11_sorting/main.py
@@ -1,21 +1,47 @@
 #http://codekata.com/kata/kata11-sorting-it-out/
-from typing import Any
-class Sorter:
-    def __init__(self):
-        self.values :list[Any] = []
+from typing import Any, Protocol
+from collections import deque
+from string import ascii_lowercase
+
+class Inserter(Protocol):
+
+    def add(self, val: Any) -> None: ...
+
+class Insertion:
+    def __init__(self, vals: deque[Any] | None = None) -> None:
+        self.vals = vals or deque()
 
     def add(self, val: Any) -> None:
-        pass
+        if not self.vals:
+            self.vals.append(val)
+            return
+        for idx, ival in enumerate(self.vals):
+            if val <= ival:
+                self.vals.insert(idx, val)
+                return
+        self.vals.append(val)
+
+class Sorter:
+    def __init__(self, ins: Inserter):
+        self.ins: Inserter =  ins
+
+    def add(self, val: str|int ) -> None:
+        if type(val) is str:
+            val = val.lower()
+            if val in ascii_lowercase:
+                return self.ins.add(val)
+            return
+        return self.ins.add(val)
 
     @property
     def balls(self) -> list[Any]:
-        return self.values
+        return list(self.ins.vals)
 
     def __str__(self) -> str:
-        return ''.join(map(str, self.values))
+        return ''.join(map(str, self.ins.vals))
 
-def test_balls():
-    sorter = Sorter()
+def test_balls(strategy: Inserter):
+    sorter = Sorter(strategy)
     assert sorter.balls == []
     sorter.add(20)
     assert sorter.balls == [20]
@@ -24,17 +50,17 @@ def test_balls():
     sorter.add(30)
     assert sorter.balls == [10, 20, 30]
 
-def test_chars():
+def test_chars(strategy: Inserter):
     in_str = '''When not studying nuclear physics, Bambi likes to play
 beach volleyball.'''
-    sorter = Sorter()
+    sorter = Sorter(strategy)
     for s in in_str:
         sorter.add(s)
     assert str(sorter) == 'aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy'
 
 def main():
-    test_balls()
-    test_chars()
+    test_balls(Insertion())
+    test_chars(Insertion())
 
 if __name__ == '__main__':
     main()
