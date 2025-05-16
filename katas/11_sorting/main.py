7c889c435aa6f4769263e16781b367e31738d19e
danny crasto <danwald79@gmail.com>
2025-05-16 11:21:53 +0400

    adds binary insert

--- katas/11_sorting/main.py
+++ katas/11_sorting/main.py
@@ -3,6 +3,8 @@ from typing import Any, Protocol
 from collections import deque
 from string import ascii_lowercase
 
+DEBUG = False
+
 class Inserter(Protocol):
 
     def add(self, val: Any) -> None: ...
@@ -20,6 +22,33 @@ class Insertion:
                 self.vals.insert(idx, val)
                 return
         self.vals.append(val)
+
+class BInserter:
+    def __init__(self, vals: deque[Any] | None = None) -> None:
+        self.vals = vals or deque()
+
+    def add(self, val: Any) -> None:
+        if not self.vals:
+            self.vals.append(val)
+            return
+        l,r = 0, len(self.vals)
+        m = (r+l) // 2
+        count = m
+        while l <  r and count:
+            cur = self.vals[m]
+            if DEBUG:
+                print(f"\t{l} <= {m} <= {r} iter:{count}")
+            if val == cur:
+                break
+            if val < cur:
+                r = m
+            else:
+                l = m
+            count -= 1
+            m = (r+l) // 2
+        if DEBUG:
+            print(f"{l} <= {m} <= {r} iter:{count} {self.vals}")
+        self.vals.insert(m+1 if val > self.vals[m] else m, val)
 
 class Sorter:
     def __init__(self, ins: Inserter):
@@ -49,6 +78,14 @@ def test_balls(strategy: Inserter):
     assert sorter.balls == [10, 20]
     sorter.add(30)
     assert sorter.balls == [10, 20, 30]
+    sorter.add(50)
+    assert sorter.balls == [10, 20, 30, 50]
+    sorter.add(10)
+    assert sorter.balls == [10, 10, 20, 30, 50]
+    sorter.add(5)
+    assert sorter.balls == [5, 10, 10, 20, 30, 50]
+    sorter.add(15)
+    assert sorter.balls == [5, 10, 10, 15, 20, 30, 50]
 
 def test_chars(strategy: Inserter):
     in_str = '''When not studying nuclear physics, Bambi likes to play
@@ -61,6 +98,8 @@ beach volleyball.'''
 def main():
     test_balls(Insertion())
     test_chars(Insertion())
+    test_balls(BInserter())
+    test_chars(BInserter())
 
 if __name__ == '__main__':
     main()
