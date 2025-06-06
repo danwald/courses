3ac1a1df0c9937ed09c585ae3fe0e3c285797f31
danny crasto <danwald79@gmail.com>
2025-06-06 14:47:13 +0400

    running but not a lot of output

--- katas/14_trigrams/main.py
+++ katas/14_trigrams/main.py
@@ -1,27 +1,33 @@
 import sys
 import argparse
-from pathlib import Path
 import random
 from collections import defaultdict
+from io import TextIOWrapper
 
 
 class TriDB(defaultdict[str, list[str]]):
-    def __init__(self, infile: Path) -> None:
+    def __init__(self, infile: TextIOWrapper) -> None:
         self.infile = infile
         super().__init__(list)
-        self.gen_db()
+        self._gen_db()
 
     def gen_key(self, a: str, b: str) -> str:
         return f"{a} {b}"
 
-    def gen_db(self) -> None:
-        with open(self.infile) as fp:
-            for line in fp.readline():
-                toks = line.strip().split()
-                for i in range(len(toks)):
-                    if i == len(toks) - 1:
-                        break
-                    self[self.gen_key(toks[i], toks[i + 1])] += toks[i + 2]
+    def _gen_db(self) -> None:
+        for line in self.infile:
+            toks = line.strip().split()
+            if len(toks) < 3:
+                continue
+            for i in range(len(toks)):
+                if i == len(toks) - 2:
+                    break
+                self[self.gen_key(toks[i], toks[i + 1])].append(toks[i + 2])
+        print(self)
+        self.infile.close()
+
+    def __str__(self) -> str:
+        return f"{len(self)} bi-grams"
 
     def get_next(self, a: str, b: str) -> str:
         if self.gen_key(a, b) in self:
@@ -29,18 +35,22 @@ class TriDB(defaultdict[str, list[str]]):
         return ""
 
     def get_random_start(self) -> tuple[str, str]:
-        key = str(random.choice(self.keys()))  # type: ignore
+        key = random.choice(list(self.keys()))
         a, b = key.split(maxsplit=1)
         return a, b
 
 
-def main(infile: Path, outfile: Path) -> None:
+def main(infile: TextIOWrapper, outfile: TextIOWrapper) -> None:
     db = TriDB(infile)
     a, b = db.get_random_start()
-    with open(outfile, "w") as fo:
+    try:
         while c := db.get_next(a, b):
-            fo.write(f"{a} {b} {c}")
+            outfile.write(f"{c} ")
             a, b = b, c
+    except Exception:
+        pass
+    finally:
+        outfile.close()
 
 
 if __name__ == "__main__":
@@ -50,15 +60,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "infile",
         nargs="?",
-        type=Path,
-        help="corpus to generate trigrams",
+        type=argparse.FileType("r"),
+        help="corpus to generate trigrams (default:stdin)",
         default=sys.stdin,
     )
     parser.add_argument(
         "outfile",
         nargs="?",
-        type=Path,
-        help="generated output from trigrams",
+        type=argparse.FileType("w"),
+        help="generated output from trigrams (default:stdout",
         default=sys.stdout,
     )
     args = parser.parse_args()
