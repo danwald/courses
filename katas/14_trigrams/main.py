a7e1c3c2549b4b075d40e790270235aeb6782aac
danny crasto <danwald79@gmail.com>
2025-06-09 10:34:26 +0400

    with backtracking and min_length options

--- katas/14_trigrams/main.py
+++ katas/14_trigrams/main.py
@@ -3,13 +3,17 @@ import argparse
 import random
 from collections import defaultdict
 from io import TextIOWrapper
+import time
 
 
 class TriDB(defaultdict[str, list[str]]):
     def __init__(self, infile: TextIOWrapper) -> None:
         self.infile = infile
         super().__init__(list)
+        start = time.perf_counter()
         self._gen_db()
+        end = time.perf_counter()
+        print(f"{self} in {end - start:.4f} seconds")
 
     def gen_key(self, a: str, b: str) -> str:
         return f"{a} {b}"
@@ -19,11 +23,8 @@ class TriDB(defaultdict[str, list[str]]):
             toks = line.strip().split()
             if len(toks) < 3:
                 continue
-            for i in range(len(toks)):
-                if i == len(toks) - 2:
-                    break
+            for i in range(len(toks) - 2):
                 self[self.gen_key(toks[i], toks[i + 1])].append(toks[i + 2])
-        print(self)
         self.infile.close()
 
     def __str__(self) -> str:
@@ -40,16 +41,31 @@ class TriDB(defaultdict[str, list[str]]):
         return a, b
 
 
-def main(infile: TextIOWrapper, outfile: TextIOWrapper) -> None:
+def main(
+    infile: TextIOWrapper,
+    outfile: TextIOWrapper,
+    min_length: int = 300,
+    backtracks: int = 10,
+) -> None:
     db = TriDB(infile)
+    bt: list[tuple[str, str]] = list()
     a, b = db.get_random_start()
     try:
-        while c := db.get_next(a, b):
+        for i in range(min_length):
+            c = db.get_next(a, b)
+            if not c:
+                backtracks -= 1
+                a, b = random.choice(bt) if bt else db.get_random_start()
+                continue
             outfile.write(f"{c} ")
+            bt.append((a, b))
             a, b = b, c
+            if not backtracks:
+                break
     except Exception:
         pass
     finally:
+        print(f"\n{i} words with {backtracks} backtracks remaining")
         outfile.close()
 
 
@@ -71,5 +87,22 @@ if __name__ == "__main__":
         help="generated output from trigrams (default:stdout",
         default=sys.stdout,
     )
+    parser.add_argument(
+        "--min-length",
+        type=int,
+        help="minimum output length (default:300)",
+        default=300,
+    )
+    parser.add_argument(
+        "--backtracks",
+        type=int,
+        help="max number of backtracks incase of not found (default:10)",
+        default=10,
+    )
     args = parser.parse_args()
-    main(args.infile, args.outfile)
+    main(
+        args.infile,
+        args.outfile,
+        min_length=args.min_length,
+        backtracks=args.backtracks,
+    )
