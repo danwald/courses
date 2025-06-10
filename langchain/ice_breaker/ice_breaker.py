cf7dc5c1d9e3cf84d842f6aac804dfe24a4f1c36
danny crasto <danwald79@gmail.com>
2025-06-10 12:18:35 +0400

    gets profile link from linked in

--- langchain/ice_breaker/ice_breaker.py
+++ langchain/ice_breaker/ice_breaker.py
@@ -1,13 +1,23 @@
+from typing import Any
+
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 from third_party.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup import lookup
+
+
+def ice_breakwith(name: str, mock: bool = False) -> Any:
+    url = lookup(name)
+    linkedin_data = scrape_linkedin_profile(url, mock=mock)
+    return linkedin_data
+
 
 if __name__ == "__main__":
-    print("Hello icebreaker")
+    print("icebreaker")
     summary_template = """
-        given the information about a person I want to create:
+        given the information {information} about a person I want to create:
         1. a short summary
         2. two interesting facts about them
     """
@@ -17,6 +27,6 @@ if __name__ == "__main__":
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     chain = prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile("https://foo/bar", mock=True)
+    linkedin_data = ice_breakwith("danny crasto dubai", mock=True)
     res = chain.invoke(input={"information": linkedin_data})
     print(res)
