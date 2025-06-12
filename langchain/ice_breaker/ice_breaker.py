e4419a03521480d6c1471ccdd494a178caaba4a7
danny crasto <danwald79@gmail.com>
2025-06-12 11:53:06 +0400

    twiiter scrappign support to icebreaker

--- langchain/ice_breaker/ice_breaker.py
+++ langchain/ice_breaker/ice_breaker.py
@@ -5,19 +5,25 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 from third_party.linkedin import scrape_linkedin_profile
+from third_party.twitter import get_user_tweets
 from agents.linkedin_lookup import lookup
+from agents.twitter_handle import lookup as twitter_lookup
 
 
 def ice_breakwith(name: str, mock: bool = False) -> Any:
-    url = lookup(name)
-    linkedin_data = scrape_linkedin_profile(url, mock=mock)
-    return linkedin_data
+    linkedin_url = lookup(name)
+    linkedin_data = scrape_linkedin_profile(linkedin_url, mock=mock)
+
+    twitter_handle = twitter_lookup(name)
+    tweets = get_user_tweets(twitter_handle, mock=mock)
+
+    return linkedin_data, tweets
 
 
 if __name__ == "__main__":
     print("icebreaker")
     summary_template = """
-        given the information {information} about a person I want to create:
+        given the information {information} about a person and {twitter_posts} want to create:
         1. a short summary
         2. two interesting facts about them
     """
@@ -27,6 +33,7 @@ if __name__ == "__main__":
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     chain = prompt_template | llm | StrOutputParser()
-    linkedin_data = ice_breakwith("danny crasto dubai", mock=True)
-    res = chain.invoke(input={"information": linkedin_data})
+
+    linkedin_data, tweets = ice_breakwith("danny crasto dubai", mock=True)
+    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
     print(res)
--- langchain/ice_breaker/third_party/twitter.py
+++ langchain/ice_breaker/third_party/twitter.py
@@ -1,19 +1,28 @@
 import os
 import requests  # type: ignore
 import tweepy
+import sys
 
 
-def get_user_tweets(handle: str, num_tweets: int = 5, mock: bool = False) -> list[str]:
+def get_user_tweets(
+    handle: str, num_tweets: int = 5, mock: bool = False
+) -> list[dict[str, str]]:
     """get twitt handerl,
     Manually scrape the information from the LinkedIn profile"""
 
-    tweets = []
+    tweet_list: list[dict[str, str]] = []
     if mock:
         gist_url = "https://gist.githubusercontent.com/emarco177/9d4fdd52dc432c72937c6e383dd1c7cc/raw/1675c4b1595ec0ddd8208544a4f915769465ed6a/eden-marco-tweets.json"
         tweets = requests.get(
             gist_url,
             timeout=10,
         ).json()
+        for tweet in tweets:
+            tweet_dict = {}
+            tweet_dict["text"] = tweet["text"]
+            tweet_dict["url"] = f"https://twitter.com/{handle}/status/{tweet['id']}"
+            tweet_list.append(tweet_dict)
+        return tweet_list
 
     else:
         client = tweepy.Client(bearer_token=os.environ["TWITTER_BEARER_TOKEN"])
@@ -29,11 +38,15 @@ def get_user_tweets(handle: str, num_tweets: int = 5, mock: bool = False) -> lis
         tweets = client.get_users_tweets(
             id=uid, max_results=num_tweets, exclude=["retweets", "replies"]
         )
-
-    return [str(t["text"]) for t in tweets[:num_tweets]]
+        for tweet in tweets.data:
+            tweet_dict = {}
+            tweet_dict["text"] = tweet["text"]
+            tweet_dict["url"] = f"https://twitter.com/{handle}/status/{tweet.id}"
+            tweet_list.append(tweet_dict)
+        return tweet_list
 
 
 if __name__ == "__main__":
     print(
-        get_user_tweets("danwald", mock=False),
+        get_user_tweets("danwald", mock=len(sys.argv) == 1),
     )
