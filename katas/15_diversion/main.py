3959543c56738b8715dc553066c433a4906bf621
danny crasto <danwald79@gmail.com>
2025-06-17 15:14:41 +0400

    fixes test cases

--- katas/15_diversion/main.py
+++ katas/15_diversion/main.py
@@ -1,13 +1,21 @@
+from itertools import pairwise
+
+
 def get_non_adj_ones(bits: int) -> int:
     if bits < 0:
         raise ValueError(f"#bits {bits} should be +ve integer")
-    mxx = 2**bits
+    mxx = 2**bits - 1
     count = 0
+    # print(bits, mxx)
     for i in range(mxx + 1):
-        while i:
-            bs = f"{i:b}"
-            print(bs)
-            i = i >> 1
+        bs = f"{i:b}"
+        for a, b in pairwise(bs):
+            if a == "1" and b == "1":
+                # print(bs)
+                break
+        else:
+            count += 1
+            # print(bs, count)
     return count
 
 
@@ -18,5 +26,7 @@ if __name__ == "__main__":
     except Exception as e:
         assert isinstance(e, ValueError)
 
-    assert 1 == get_non_adj_ones(1)
+    assert 2 == get_non_adj_ones(1)
     assert 3 == get_non_adj_ones(2)
+    assert 5 == get_non_adj_ones(3)
+    assert 8 == get_non_adj_ones(4)
