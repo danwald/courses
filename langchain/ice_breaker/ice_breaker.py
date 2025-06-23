417e4e55c69380cbb28b9e7c8f6a9ae272473158
danny crasto <danwald79@gmail.com>
2025-06-23 11:34:01 +0400

    icebreaker releasing returning summary/url

--- langchain/ice_breaker/ice_breaker.py
+++ langchain/ice_breaker/ice_breaker.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
@@ -7,21 +5,16 @@ from third_party.linkedin import scrape_linkedin_profile
 from third_party.twitter import get_user_tweets
 from agents.linkedin_lookup import lookup
 from agents.twitter_handle import lookup as twitter_lookup
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 
 
-def ice_breakwith(name: str, mock: bool = False) -> Any:
+def ice_breakwith(name: str, mock: bool = False) -> tuple[Summary, str]:
     linkedin_url = lookup(name)
     linkedin_data = scrape_linkedin_profile(linkedin_url, mock=mock)
 
     twitter_handle = twitter_lookup(name)
     tweets = get_user_tweets(twitter_handle, mock=mock)
 
-    return linkedin_data, tweets
-
-
-if __name__ == "__main__":
-    print("icebreaker")
     summary_template = """
         given the linkedin information {information} about a person and their latest twitter posts {twitter_posts} want to create:
         1. a short summary
@@ -40,6 +33,13 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     chain = prompt_template | llm | summary_parser
 
-    linkedin_data, tweets = ice_breakwith("danny crasto dubai", mock=True)
-    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
-    print(res)
+    res: Summary = chain.invoke(
+        input={"information": linkedin_data, "twitter_posts": tweets}
+    )
+
+    return res, linkedin_data.get("photoUrl", "")
+
+
+if __name__ == "__main__":
+    print("icebreaker")
+    ice_breakwith("danny crasto dubai")
