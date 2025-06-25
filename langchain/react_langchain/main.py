a294debddfed0c28be7bdf3ed9d8e3812f4b73b4
danny crasto <danwald79@gmail.com>
2025-06-25 11:50:24 +0400

    add option to ruff formatter

--- langchain/react_langchain/main.py
+++ langchain/react_langchain/main.py
@@ -4,10 +4,11 @@ from langchain.agents import tool
 @tool  # type: ignore
 def get_text_length(text: str) -> int:
     """Returns  the length of text by characters"""
-    return len(text)
+    return len(text.strip("\n").strip('"'))
 
 
 if __name__ == "__main__":
     txt = "Hello React Langchain"
     result = get_text_length.invoke({"text": txt})
+    tools = [get_text_length]
     print(f"'{txt}' is {result} characters long")
