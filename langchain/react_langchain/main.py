086cc6b8547733e58502d2dd67ffc6ba364f7fb9
danny crasto <danwald79@gmail.com>
2025-06-30 11:52:52 +0400

    add tool support

--- langchain/react_langchain/main.py
+++ langchain/react_langchain/main.py
@@ -1,15 +1,21 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_core.tools.render import render_text_description
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_openai import ChatOpenAI
-
+from langchain.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
 from langchain.agents import tool
 
 
 @tool  # type: ignore
 def get_text_length(text: str) -> int:
     """Returns  the length of text by characters"""
-    return len(text.strip("\n").strip('"'))
+    return len(text.strip("\n").strip('"').strip("'"))
 
+def find_tool_by_name(name: str, tools: [str]) -> str:
+    for tll in tools:
+        if tll.name == name:
+            return tll
+    raise ValueError(f'Tool:{name} not found')
 
 if __name__ == "__main__":
     txt = "Hello React Langchain"
@@ -34,9 +40,17 @@ if __name__ == "__main__":
     Begin!
 
     Question: {input}
-    Thought:{agent_scratchpad}
+    Thought:
     """
     prompt = PromptTemplate(template=template).partial(
         tools=render_text_description(tools), tool_names=",".join(t.name for t in tools)
     )
     llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
+    agent = {"input": lambda x: x['input']} | prompt | llm | ReActSingleInputOutputParser()
+    agent_step: [AgentAction | AgentFinish] = agent.invoke({"input": "What's the length of the word 'DOG'?"})
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tooled = find_tool_by_name(tool_name, tools)
+        tool_input = agent_step.tool_input
+        obs = tooled.func(tool_input)
+        print(obs)
