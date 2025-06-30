68e9246ae37c3a3238cd990214cefe82dbd1c003
danny crasto <danwald79@gmail.com>
2025-06-30 12:25:05 +0400

    adds scratchpad wiht thoughts and agent finish

--- langchain/react_langchain/main.py
+++ langchain/react_langchain/main.py
@@ -6,6 +6,14 @@ from langchain.agents.output_parsers.react_single_input import ReActSingleInputO
 from langchain.agents import tool
 
 
+def format_log_to_str(steps: [tuple[AgentAction, str]], obs_prefix: str='Obersvation: ', llm_prefix:str='Thought: ') -> str:
+    thoughts: str = ''
+    for action, observation in steps:
+        thoughts += action.log
+        thoughts += f"{obs_prefix}{observation}\n{llm_prefix}"
+    return thoughts
+
+
 @tool  # type: ignore
 def get_text_length(text: str) -> int:
     """Returns  the length of text by characters"""
@@ -40,17 +48,41 @@ if __name__ == "__main__":
     Begin!
 
     Question: {input}
-    Thought:
+    Thought: {agent_scratchpad}
     """
     prompt = PromptTemplate(template=template).partial(
         tools=render_text_description(tools), tool_names=",".join(t.name for t in tools)
     )
     llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
-    agent = {"input": lambda x: x['input']} | prompt | llm | ReActSingleInputOutputParser()
-    agent_step: [AgentAction | AgentFinish] = agent.invoke({"input": "What's the length of the word 'DOG'?"})
+    intermediate_steps = []
+    agent = (
+        {
+            "input": lambda x: x['input'],
+            "agent_scratchpad": lambda x: format_log_to_str(x['agent_scratchpad']),
+        } |
+        prompt |
+        llm |
+        ReActSingleInputOutputParser()
+    )
+    agent_step: [AgentAction | AgentFinish] = agent.invoke(
+        {
+            "input": "What's the length of the word DOG?",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
     if isinstance(agent_step, AgentAction):
         tool_name = agent_step.tool
         tooled = find_tool_by_name(tool_name, tools)
         tool_input = agent_step.tool_input
         obs = tooled.func(tool_input)
         print(obs)
+        intermediate_steps.append((agent_step, str(obs)))
+
+    agent_step: [AgentAction | AgentFinish] = agent.invoke(
+        {
+            "input": "What's the length of the word DOG?",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+    if isinstance(agent_step, AgentFinish):
+        print(agent_step.return_values)
