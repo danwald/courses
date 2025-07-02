62ea589f5ee7acc7806f98fe25d2c3b6404348a0
danny crasto <danwald79@gmail.com>
2025-07-02 12:19:47 +0400

    add levenshtein distance measure

---
+++ katas/19_wordchains/ldistance.py
@@ -0,0 +1,26 @@
+def distance(s: str, t: str) -> int:
+    # https://en.wikipedia.org/wiki/Levenshtein_distance#Definition
+    if not s:
+        return len(t)
+    if not t:
+        return len(s)
+
+    if s[0] == t[0]:
+        return distance(s[1:], t[1:])
+
+    return 1 + min(
+        distance(s[1:], t),
+        distance(s, t[1:]),
+        distance(s[1:], t[1:]),
+    )
+
+
+if __name__ == "__main__":
+    assert distance("foo", "foo") == 0
+    assert distance("kitten", "sitting") == 3
+    assert distance("foo", "bar") == 3
+    assert distance("baz", "bar") == 1
+    assert distance("foo", "") == 3
+    assert distance("", "bar") == 3
+    assert distance("foo", "far") == 2
+    assert distance("", "") == 0
