cbf7e634ab723333e60b6da684e0c7387b97a5fd
danny crasto <danwald79@gmail.com>
2025-07-05 11:59:16 +0400

    wip

--- katas/19_wordchains/main.py
+++ katas/19_wordchains/main.py
@@ -4,12 +4,18 @@ from collections import defaultdict
 from pathlib import Path
 
 
-class Words(defaultdict[int, list[str]]):
+class Words(defaultdict[int, set[str]]):
     def __init__(self, path: Path = Path("../data/worlist.txt")) -> None:
+        super().__init__(set)
         self.path = path
+        if not path.exists():
+            raise ValueError(f"Invalid file path {self.path}")
 
     def process(self) -> None:
-        pass
+        with codecs.open(self.path, "r", encoding="utf-8", errors="ignore") as fp:
+            for line in fp:
+                for word in line.strip().split(" "):
+                    self[len(word)].add(word)
 
     def __str__(self) -> str:
         if not self:
@@ -17,30 +23,20 @@ class Words(defaultdict[int, list[str]]):
         return f"dictionary size length {sum(len(words) for words in self.values())}"
 
 
-def dict_word_len_grouped(
-    path: str = "../data/wordlist.txt",
-) -> defaultdict[int, set[str]]:
-    dt: defaultdict[int, set[str]] = defaultdict(set)
-    with codecs.open(path, "r", encoding="utf-8", errors="ignore") as fp:
-        for line in fp:
-            for word in line.strip().split(" "):
-                dt[len(word)].add(word)
-    return dt
-
 
 def main() -> None:
-    d = dict_word_len_grouped()
-    print(f"dictionary size length {sum(len(words) for words in d.values())}")
+    d = Words()
+    print(f"{d}")
 
 
 def tests() -> None:
     with tempfile.NamedTemporaryFile(mode="w+") as tmp:
         tmp.write("cat\ncog\ncot\ndog\n")
         tmp.flush()
-        td = dict_word_len_grouped(tmp.name)
-        print(f"dictionary size length {sum(len(words) for words in td.values())}")
+        td = Words(Path(tmp.name))
+        print(f"{td}")
 
 
 if __name__ == "__main__":
     tests()
-    # main()
+    main()
