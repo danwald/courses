c5b7adf70d7d917caf97037286dbd4847903d7e5
danny crasto <danwald79@gmail.com>
2025-07-29 11:44:22 +0400

    messing with types

--- ts/vanilla/src/tuitorial.ts
+++ ts/vanilla/src/tuitorial.ts
@@ -1,1 +1,16 @@
 console.log('boo!');
+
+let awesomeName:string = 'foobar';
+awesomeName.toUpperCase();
+console.log(awesomeName);
+
+let amount = 100;
+//amount += '10';
+amount += 10;
+console.log(amount);
+
+
+//let isAwe:boolean = 1;
+let isAwe:boolean = true;
+isAwe = false;
+console.log(isAwe);
