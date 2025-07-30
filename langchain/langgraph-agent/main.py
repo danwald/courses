046f61ece602fd492f201f925c36ea373d84e329
danny crasto <danwald79@gmail.com>
2025-07-30 18:04:24 +0400

    runs the agent graph

--- langchain/langgraph-agent/main.py
+++ langchain/langgraph-agent/main.py
@@ -1,4 +1,44 @@
-import lang
+import os
+from typing import Any
 
-if __name__ == '__main__':
-    print('hello')
+from langchain_core.messages import HumanMessage
+from langgraph.graph import END, MessagesState, StateGraph
+from nodes import run_agent_reasoning, tool_node
+
+AGENT_REASON = "agent_reason"
+ACT = "act"
+LAST = -1
+
+
+def should_continue(state: MessagesState) -> Any:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {END: END, ACT: ACT})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
+if __name__ == "__main__":
+    print("Hello ReAct LangGraph with Function Calling")
+    os.environ["LANGSMITH_PROJECT"] = "react-agent"
+    res = app.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="What is the temperature in Tokyo? List it and then triple it"
+                )
+            ]
+        }
+    )
+    print(res["messages"][LAST].content)
