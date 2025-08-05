6c7df017f696f0721c153a3d61e477f515185539
danny crasto <danwald79@gmail.com>
2025-08-05 11:43:23 +0400

    update main to run

--- langchain/reflexion-agent/main.py
+++ langchain/reflexion-agent/main.py
@@ -1,11 +1,43 @@
 import os
-from typing import List, Sequence
-from langchain_core.messages import BaseMessage, HumanMessage
+from typing import List
+from langchain_core.messages import BaseMessage,ToolMessage
 from langgraph.graph import END, MessageGraph
 
-from chains import generate_chain, reflect_chain
+from chains import first_responder, revisor
+from tool_executor import execute_tools
+
+MAX_ITERATIONS = 2
+
+def event_loop(state: List[BaseMessage]) -> str:
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
+
 
 if __name__ == "__main__":
     print('hello reflexion agent')
     os.environ["LANGSMITH_PROJECT"] = "reflexion-agent"
-    response = graph.invoke(inputs)
+
+    builder = MessageGraph()
+    builder.add_node("draft", first_responder)
+    builder.add_node("execute_tools", execute_tools)
+    builder.add_node("revise", revisor)
+    builder.add_edge("draft", "execute_tools")
+    builder.add_edge("execute_tools", "revise")
+
+
+
+    builder.add_conditional_edges("revise", event_loop)
+    builder.set_entry_point("draft")
+    graph = builder.compile()
+
+    print(graph.get_graph().draw_mermaid())
+
+
+    res = graph.invoke(
+        "Write about AI-Powered SOC / autonomous soc  problem domain, list startups that do that and raised capital."
+    )
+    print(res[-1].tool_calls[0]["args"]["answer"])
+    print(res)
