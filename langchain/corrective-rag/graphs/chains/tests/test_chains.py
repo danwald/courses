d31a7f7d64c09e01ccc6ad14b10454530cd361ef
danny crasto <danwald79@gmail.com>
2025-08-07 14:43:14 +0400

    adds test

--- langchain/corrective-rag/graphs/chains/tests/test_chains.py
+++ langchain/corrective-rag/graphs/chains/tests/test_chains.py
@@ -1,3 +1,18 @@
+from graphs.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from ingestion import retriever
+import pytest
 
-def test_chains():
-    assert True
+@pytest.mark.parametrize(("question", "expected"), (
+    ("agent memory", "yes"),
+    ("how can I make chicago deepdish pizza", "no"),
+))
+def test_retrival_grader(question:str, expected:str) -> None:
+    question = question
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke({
+        "document": doc_txt,
+        "question": question,
+    })
+    assert res.binary_score == expected
