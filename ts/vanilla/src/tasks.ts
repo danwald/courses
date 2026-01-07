a71ce8e4f7f6ab2b5b09b810dd4aab71944e2fff
danny crasto <danwald79@gmail.com>
2026-01-07 07:23:47 +0400

    updates to task app adding tasks

--- ts/vanilla/src/tasks.ts
+++ ts/vanilla/src/tasks.ts
@@ -1,28 +1,33 @@
-const btn = document.querySelector<HTMLButtonElement>('.test-btn');
+const taskForm = document.querySelector<HTMLFormElement>('.form')!;
+const inputForm = document.querySelector<HTMLFormElement>('.form-input')!;
+const taskListElement = document.querySelector<HTMLUListElement>('.list')!;
 
-const taskForm = document.querySelector<HTMLFormElement>('.form-input');
-const list = document.querySelector<HTMLUListElement>('.list');
 
-console.log(`form: ${taskForm}`);
-if (btn) {
-    btn.disabled = true;
-}
 
 type Task = {
-    description: string;
-    isComplete: boolean;
-}
+  description: string;
+  isComplete: boolean;
+};
 
 const tasks: Task[] = [];
+function submitFunc(event: SubmitEvent) {
+  event.preventDefault();
+  const taskDescription = inputForm?.value;
+  if (!taskDescription) {
+    alert('Please add a description before sumbit');
+    return
+  }
+  const task: Task = {
+    description: taskDescription,
+    isComplete: true,
+  }
+  addTask(task);
+  inputForm.value = ''
 
-taskForm?.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const desc = taskForm?.value;
-    console.log(`Desc: ${desc}`);
-    if(!desc){
-        alert('Please add a description before sumbit');
-        return
-    }
-    //tasks.append(new Task(desc), random.random());
+}
+function addTask(task: Task): void {
+  tasks.push(task);
+  console.log(tasks);
+};
 
-});
+taskForm.addEventListener('submit', submitFunc);
