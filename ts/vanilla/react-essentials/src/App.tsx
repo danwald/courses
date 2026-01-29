65eb28ae17c4cca07d3faf5fbfdd63c7f3d194fa
danny crasto <danwald79@gmail.com>
2026-01-29 16:43:07 +0400

    manuals adds props with chilrend

--- ts/vanilla/react-essentials/src/App.tsx
+++ ts/vanilla/react-essentials/src/App.tsx
@@ -4,7 +4,9 @@ import Component from './starter/02-props/'
 function App() {
   return (
     <main>
-      <Component name='Danny' id={123} />
+      <Component name='Danny' id={123}>
+        <h3> befriends</h3>
+      </Component>
       <Component name='Thanda' id={345} />
     </main>
   );
--- ts/vanilla/react-essentials/src/starter/02-props/index.tsx
+++ ts/vanilla/react-essentials/src/starter/02-props/index.tsx
@@ -1,14 +1,15 @@
 type ComponentProps = {
   name: string;
   id: number;
-
+  children?: React.ReactNode;
 }
 
-function Component({ name, id }: ComponentProps) {
+function Component({ name, id, children }: ComponentProps) {
   return (
     <div>
       <h2>Name: {name}</h2>
       <h2>Id {id}</h2>
+      {children}
     </div>
   );
 }
