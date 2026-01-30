bd6fb1d3a69697a2bf3385de247c72035a90e9dc
danny crasto <danwald79@gmail.com>
2026-01-30 19:00:56 +0400

    using state via UseState

--- ts/vanilla/react-essentials/src/starter/03-state/index.tsx
+++ ts/vanilla/react-essentials/src/starter/03-state/index.tsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
 
-
+const navlinks: Link[] = [
+  {
+    id: 1,
+    url: 'some url',
+    text: 'some text',
+  },
+  {
+    id: 2,
+    url: 'some url',
+    text: 'some text',
+  },
+  {
+    id: 3,
+    url: 'some url',
+    text: 'some text',
+  },
+]
+type Link = {
+  id: number;
+  url: string;
+  text: string;
+}
 function Component() {
   const [text, setText] = useState<string>("shakeAndBake");
+  const [links, setLinks] = useState<Link[]>(navlinks);
   return (
     <div>
       <h2 className="mb-1">React & Typescript</h2>
-      <button className="btn btn-center" onClick={() => { setText("foobar") }}>Click Me</button>
+      <button className="btn btn-center"
+        onClick={() => {
+          setText("foobar");
+          setLinks([...navlinks, { id: 4, text: 'some text', url: 'some url' }]);
+        }} >Click Me</button>
     </div >
   );
 }
