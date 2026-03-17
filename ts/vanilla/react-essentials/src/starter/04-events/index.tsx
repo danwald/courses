d07c66de68378bc9567fac03050172db175a3fda
danny crasto <danwald79@gmail.com>
2026-03-17 21:15:52 +0400

    events 04

--- ts/vanilla/react-essentials/src/starter/04-events/index.tsx
+++ ts/vanilla/react-essentials/src/starter/04-events/index.tsx
@@ -4,27 +4,35 @@ function Component() {
   const [text, setText] = useState("");
   const [email, setEmail] = useState("");
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  };
   return (
-    <div>
+    <section>
       <h2>Events example</h2>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="form-input mb-1"
           value={text}
           onChange={(e) => setText(e.target.value)}
+          name="text"
         />
         <input
           type="email"
           className="form-input mb-1"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          name="email"
         />
         <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-    </div>
+    </section>
   );
 }
 
