2d03c14058534d39aef1515812dc14e86e695f81
danny crasto <danwald79@gmail.com>
2026-09-07 21:46:50 +0400

    adding zod schema

--- nextjs/nextjs-dashboard/app/lib/action.ts
+++ nextjs/nextjs-dashboard/app/lib/action.ts
@@ -1,11 +1,19 @@
 "use server";
+import { z } from "zod";
 
+const FontSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FontSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
 }
