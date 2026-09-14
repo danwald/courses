a874300a0592ade795baf0cd1dce4cc409e602f4
danny crasto <danwald79@gmail.com>
2026-09-14 21:40:28 +0400

    updated invoce

--- nextjs/nextjs-dashboard/app/lib/action.ts
+++ nextjs/nextjs-dashboard/app/lib/action.ts
@@ -7,7 +7,7 @@ import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-const FontSchema = z.object({
+const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
@@ -15,8 +15,10 @@ const FontSchema = z.object({
   date: z.string(),
 });
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const CreateInvoice = FontSchema.omit({ id: true, date: true });
+  const CreateInvoice = FormSchema.omit({ id: true, date: true });
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -29,6 +31,25 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
